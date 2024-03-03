a3f207237285d4ff9c9701e14c75bc5197abf931
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-03-03 18:00:45 -0500

    add: data - id

--- dist/index.js
+++ dist/index.js
@@ -2,26 +2,31 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 const books = [
     {
+        id: 1,
         title: 'The Great Gatsby',
         author: 'F. Scott Fitzgerald',
     },
     {
+        id: 2,
         title: 'The Da Vinci Code',
         author: 'Dan Brown',
     },
 ];
 const typeDefs = `
     type Book {
+		id: ID!
         title: String
         author: String
     }
     type Query {
         books: [Book]
+		book(id: ID!): Book
     }
 `;
 const resolvers = {
     Query: {
         books: () => books,
+        book: (parent, args) => books.find(book => book.id === parseInt(args.id)),
     },
 };
 const server = new ApolloServer({ typeDefs, resolvers });
