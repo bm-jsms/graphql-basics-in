5698fef7ea1286b8b5974655c39d7bd6e0986013
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-03-02 19:09:14 -0500

    start

---
+++ dist/index.js
@@ -0,0 +1,31 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+const books = [
+    {
+        title: 'The Great Gatsby',
+        author: 'F. Scott Fitzgerald',
+    },
+    {
+        title: 'The Da Vinci Code',
+        author: 'Dan Brown',
+    },
+];
+const typeDefs = `
+    type Book {
+        title: String
+        author: String
+    }
+    type Query {
+        books: [Book]
+    }
+`;
+const resolvers = {
+    Query: {
+        books: () => books,
+    },
+};
+const server = new ApolloServer({ typeDefs, resolvers });
+const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+});
+console.log(`Server ready at ${url}`);
