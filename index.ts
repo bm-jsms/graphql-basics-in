adb46877c502f59c78e9f1b036eb3bc2699360e6
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-03-02 19:05:21 -0500

    run server

--- index.ts
+++ index.ts
@@ -34,3 +34,5 @@ const server = new ApolloServer({ typeDefs, resolvers });
 const { url } = await startStandaloneServer(server, {
 	listen: { port: 4000 },
 });
+
+console.log(`Server ready at ${url}`);
